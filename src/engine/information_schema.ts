b8b4a37798b900832724a2ed7c66f1785e0bede2
Weidong Fang <weidong@sinoease.com>
2019-04-18 12:02:42 +0930

    Fixed bug on some versions of Mac

--- src/engine/information_schema.ts
+++ src/engine/information_schema.ts
@@ -82,108 +82,117 @@ class Builder {
   }
 
   getTables(): Promise<Set<string>> {
-    return this.connection
-      .query(
-        `
+    return query(
+      this.connection,
+      `
         select table_name from information_schema.tables
         where table_schema = ${
           this.escapedSchemaName
         } and table_type = 'BASE TABLE'
         `
-      )
-      .then(rows => {
-        const set = new Set();
-        for (const row of rows) {
-          set.add(row.table_name);
-        }
-        return set;
-      });
+    ).then(rows => {
+      const set = new Set();
+      for (const row of rows) {
+        set.add(row.table_name);
+      }
+      return set;
+    });
   }
 
   getColumns(): Promise<{ [key: string]: ColumnInfo[] }> {
-    return this.connection
-      .query(
-        `
+    return query(
+      this.connection,
+      `
         select table_name, column_name, ordinal_position, column_default,
         is_nullable, data_type, character_maximum_length, extra
         from information_schema.columns
         where table_schema = ${this.escapedSchemaName}`
-      )
-      .then(rows => {
-        const map = {};
-        for (const row of rows) {
-          map[row.table_name] = map[row.table_name] || [];
-          const columnInfo: ColumnInfo = {
-            name: row.column_name,
-            type: row.data_type,
-            nullable: row.is_nullable === 'YES'
-          };
-          if (/char|text/i.exec(columnInfo.type)) {
-            columnInfo.size = row.character_maximum_length;
-          }
-          if (/auto_increment/i.exec(row.extra)) {
-            columnInfo.autoIncrement = true;
-          }
-          map[row.table_name].push([row.ordinal_position, columnInfo]);
+    ).then(rows => {
+      const map = {};
+      for (const row of rows) {
+        map[row.table_name] = map[row.table_name] || [];
+        const columnInfo: ColumnInfo = {
+          name: row.column_name,
+          type: row.data_type,
+          nullable: row.is_nullable === 'YES'
+        };
+        if (/char|text/i.exec(columnInfo.type)) {
+          columnInfo.size = row.character_maximum_length;
         }
-        for (const tableName in map) {
-          const columns = map[tableName];
-          map[tableName] = columns.sort((a, b) => a[0] - b[0]).map(r => r[1]);
+        if (/auto_increment/i.exec(row.extra)) {
+          columnInfo.autoIncrement = true;
         }
-        return map;
-      });
+        map[row.table_name].push([row.ordinal_position, columnInfo]);
+      }
+      for (const tableName in map) {
+        const columns = map[tableName];
+        map[tableName] = columns.sort((a, b) => a[0] - b[0]).map(r => r[1]);
+      }
+      return map;
+    });
   }
 
   // table_name => constraint_name => constraint_type
   getTableConstraints(): Promise<{ [key: string]: { [key: string]: string } }> {
-    return this.connection
-      .query(
-        `
+    return query(
+      this.connection,
+      `
         select table_name, constraint_name, constraint_type
         from information_schema.table_constraints
         where table_schema = ${this.escapedSchemaName}`
-      )
-      .then(rows => {
-        const map = {};
-        for (const row of rows) {
-          map[row.table_name] = map[row.table_name] || {};
-          map[row.table_name][row.constraint_name] = row.constraint_type;
-        }
-        return map;
-      });
+    ).then(rows => {
+      const map = {};
+      for (let row of rows) {
+        row = lower(row);
+        map[row.table_name] = map[row.table_name] || {};
+        map[row.table_name][row.constraint_name] = row.constraint_type;
+      }
+      return map;
+    });
   }
 
   // table_name => constraint_name => column_name[]
   getKeyColumnUsage(): Promise<{ [key: string]: { [key: string]: string[] } }> {
-    return this.connection
-      .query(
-        `
+    return query(
+      this.connection,
+      `
         select table_name, constraint_name, column_name, ordinal_position,
         referenced_table_name, referenced_column_name
         from information_schema.key_column_usage
         where table_schema = ${this.escapedSchemaName}`
-      )
-      .then(rows => {
-        const map = {};
-        for (const row of rows) {
-          map[row.table_name] = map[row.table_name] || {};
-          map[row.table_name][row.constraint_name] =
-            map[row.table_name][row.constraint_name] || [];
-          map[row.table_name][row.constraint_name].push([
-            row.ordinal_position,
-            row.column_name,
-            [row.referenced_table_name, row.referenced_column_name]
-          ]);
+    ).then(rows => {
+      const map = {};
+      for (const row of rows) {
+        map[row.table_name] = map[row.table_name] || {};
+        map[row.table_name][row.constraint_name] =
+          map[row.table_name][row.constraint_name] || [];
+        map[row.table_name][row.constraint_name].push([
+          row.ordinal_position,
+          row.column_name,
+          [row.referenced_table_name, row.referenced_column_name]
+        ]);
+      }
+      for (const tableName in map) {
+        for (const constraintName in map[tableName]) {
+          const columns = map[tableName][constraintName];
+          map[tableName][constraintName] = columns
+            .sort((a, b) => a[0] - b[0])
+            .map(r => [r[1], r[2]]);
         }
-        for (const tableName in map) {
-          for (const constraintName in map[tableName]) {
-            const columns = map[tableName][constraintName];
-            map[tableName][constraintName] = columns
-              .sort((a, b) => a[0] - b[0])
-              .map(r => [r[1], r[2]]);
-          }
-        }
-        return map;
-      });
+      }
+      return map;
+    });
   }
+}
+
+function lower(row: { [key: string]: any }) {
+  const result = {};
+  for (const key in row) {
+    result[key.toLowerCase()] = row[key];
+  }
+  return result;
+}
+
+function query(connection: Connection, query: string) {
+  return connection.query(query).then(rows => rows.map(row => lower(row)));
 }
